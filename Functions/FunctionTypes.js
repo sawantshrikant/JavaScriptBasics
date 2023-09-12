14c6a175c904dab9b64675c1e0dc20379e25d958
Shrikant <sawantshrikant07@gmail.com>
2023-09-12 23:57:28 +0530

    [Shrikant] Add:Functions In Java Script

---
+++ Functions/FunctionTypes.js
@@ -0,0 +1,24 @@
+//normal function or also called traditional function
+function greetings(name){
+    return "Hello, "+ name + "!";
+}
+
+let greetingMessage = greetings("shrikant")
+console.log(greetingMessage)
+
+function add(a, b) {
+    return a + b;
+  }
+
+// Arrow Function
+
+const multiply = (a,b) => a * b;
+console.log(multiply(4, 6));
+
+//Function Expression
+const SumOf2Numbers = function(a, b) {
+    return a + b;
+  };
+const result = SumOf2Numbers(5, 3); 
+console.log(result);
+
