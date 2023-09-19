66f7ebe5f4ef2f38820276838a8291b33473178e
Shrikant <sawantshrikant07@gmail.com>
2023-09-19 22:26:17 +0530

    [Shrikant] Add: replace, replaceAll inbuild string method

--- StringMethods/replace/replace.js
+++ StringMethods/replace/replace.js
@@ -1,3 +1,12 @@
 const string = 'Hello, World!';
 const newString = string.replace('World', 'Universe');
 console.log(newString);
+
+//replaceAll
+const line = 'Hello, World! Hello, Universe!';
+const result = line.replaceAll('Hello', 'Hi');
+console.log(result);
+
+const line2 = "amit help poor people,amit not help rich people" 
+const result1 =line2.replaceAll('amit', 'shrikant')
+console.log(result1);
