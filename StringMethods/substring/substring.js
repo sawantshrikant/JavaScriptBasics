a98ea802e60c01d926e5bae8eff7250af3b81d8e
Shrikant <sawantshrikant07@gmail.com>
2023-09-19 22:01:18 +0530

    [Shrikant] Add: sunstring inbuild string method

---
+++ StringMethods/substring/substring.js
@@ -0,0 +1,23 @@
+const text = "Hello, world!";
+const substring = text.substring(0, 5);
+console.log(substring);
+
+//Omitting the endIndex:
+
+const text1 = "Hello, this is shrikant sawant on this side";
+const substring1 = text1.substring(7);
+console.log(substring1);
+
+//Swapping Indices
+//You can also swap the order of the indices to extract characters in reverse order
+
+const name1 = "sds is a good person";
+const sunstring3 = name1.substring(5,0);
+console.log(sunstring3);
+
+//Using Negative Indices
+//Negative indices count from the end of the string
+
+const line = "sds is a good person";
+const sunstring4 = line.substring(-3);
+console.log(sunstring4);
